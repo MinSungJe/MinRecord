5142ae243be884bc23b04c9fefc83a19886d89b0
Minje Sung <minje813@dgu.ac.kr>
2025-04-09 16:07:55 +0900

    refactor: whisper 이용 코드 함수형으로 변경

--- whisper/main.py
+++ whisper/main.py
@@ -1,5 +1,9 @@
 import whisper
 
-model = whisper.load_model("turbo")
-result = model.transcribe("record\\record.m4a")
-print(result["text"])
+def get_text_from_record():
+    model = whisper.load_model("turbo")
+    result = model.transcribe("record\\record.m4a")
+    return result["text"]
+
+if __name__ == "__main__":
+    get_text_from_record()
